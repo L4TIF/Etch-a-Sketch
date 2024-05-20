caba6201832420f31d7e67b1d7a3771f24717437
Latiful Rahman <latif.fiverr@gmail.com>
2024-05-20 15:52:52 +0000

    add grid function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // const pixels = parseInt(prompt("Enter no. of pixels"));
+const container = document.querySelector(".container");
+
+
+//how many grids?
+let createGrid = (gridCount = 16) => {
+    for (let i = 0; i < gridCount * gridCount; i++) {
+        const grid = document.createElement("div");
+        grid.style.height = `${100 / gridCount}%`;
+        grid.style.width = `${100 / gridCount}%`;
+        container.appendChild(grid);
+    }
+}
+createGrid();
