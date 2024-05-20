beb1947982aeeb58ede48aed40e7f6d0e90fd9c6
Latiful Rahman <latif.fiverr@gmail.com>
2024-05-20 16:33:38 +0000

    add btn

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// const pixels = parseInt(prompt("Enter no. of pixels"));
 const container = document.querySelector(".container");
-
+const gridBtn = document.querySelector(".grid-btn");
 
 //how many grids?
 let createGrid = (gridCount = 16) => {
+    console.log(gridCount)
     for (let i = 0; i < gridCount * gridCount; i++) {
         const grid = document.createElement("div");
         grid.style.height = `${100 / gridCount}%`;
@@ -11,4 +11,17 @@ let createGrid = (gridCount = 16) => {
         container.appendChild(grid);
     }
 }
-createGrid();
+createGrid(); //create 16*16 grid first time
+
+//clean grid
+let cleanGrid = () => {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+gridBtn.addEventListener("click", () => {
+    const pixels = parseInt(prompt("Enter no. of pixels"));
+    cleanGrid();
+    createGrid(pixels);
+})
