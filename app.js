1c4f49906e40805e2b5d4445d1d4a4ff7a37a918
Latiful Rahman <latif.fiverr@gmail.com>
2024-05-21 12:38:07 +0000

    add colorPicker, resetbtn, hover

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 const container = document.querySelector(".container");
 const gridBtn = document.querySelector(".grid-btn");
+const resetBtn = document.querySelector(".reset");
+const colorPicker = document.querySelector(".color");
 
-//how many grids?
-let createGrid = (gridCount = 16) => {
-    console.log(gridCount)
-    for (let i = 0; i < gridCount * gridCount; i++) {
-        const grid = document.createElement("div");
-        grid.style.height = `${100 / gridCount}%`;
-        grid.style.width = `${100 / gridCount}%`;
-        container.appendChild(grid);
-    }
-}
-createGrid(); //create 16*16 grid first time
 
-//clean grid
-let cleanGrid = () => {
+
+//delete container grids for new one
+let deleteGrid = () => {
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
     }
 }
 
 gridBtn.addEventListener("click", () => {
-    const pixels = parseInt(prompt("Enter no. of pixels"));
-    cleanGrid();
-    createGrid(pixels);
+    const pixels = parseInt(prompt("Enter no. of pixels should not be more than 100"));
+    if (!isNaN(pixels) && pixels <= 100) {
+        deleteGrid(); //delete prev grid
+        createGrid(pixels);
+    }
 })
+
+
+// draw 
+const draw = (grid) => {
+    let target;
+    grid.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = colorPicker.value;
+        target = e.target;
+    })
+    // reset grid
+    resetBtn.addEventListener("click", () => {
+        target.style.backgroundColor = "white";
+    })
+
+
+}
+
+
+//how many grids?
+let createGrid = (gridCount = 16) => {
+    console.log(gridCount)
+    for (let i = 0; i < gridCount * gridCount; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.style.height = `${100 / gridCount}%`;
+        grid.style.width = `${100 / gridCount}%`;
+        container.appendChild(grid);
+        draw(grid);
+    }
+}
+createGrid(); //create 16*16 grid first time
